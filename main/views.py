adc07ca9302146a474a35b49905057140e22cd18
kout112a <koutan5121kd@gmail.com>
2025-09-13 18:30:15 +0900

    テンプレート作ったよ

--- main/views.py
+++ main/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 # 関数ベースビュー
 # ビュー関数
 def index (request):
-    return HttpResponse("時間と金がほしいわ")
+    question_list = [
+        "生きている意味は何だ",
+        "お前の住所は何だ",
+        "銀行口座の番号は何だ",
+    ]
+    context = {
+        "question_list": question_list,
+        "is_polled": True,
+        "polled_msg": "(￣ー￣)((´∀｀))ｹﾗｹﾗ",
+        "not_polled_msg":"(* ´艸｀)ｸｽｸｽ",
+        "user-name":"hello"
+    }
+    return render(request, "main/index.html",context,)
